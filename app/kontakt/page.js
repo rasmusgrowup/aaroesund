9226145400ee69d3aac8691b9e8fe631599e4410
Rasmus Andersen <rasmus@growupstudio.dk>
2025-09-06 19:24:40 +0200

    Small fixes

--- app/kontakt/page.js
+++ app/kontakt/page.js
@@ -9,23 +9,23 @@ export default function KontaktPage() {
                 <li className={"mb-6"}>
                     <h3 className={"font-bold"}>Formand</h3>
                     <p>Jørgen Platz</p>
-                    <Link href={"mailto: faergevej106@gmail.com"}>faergevej106@gmail.com</Link>
+                    <Link className={"block underline"} href={"mailto: faergevej106@gmail.com"}>faergevej106@gmail.com</Link>
                 </li>
                 <li className={"mb-6"}>
                     <h3 className={"font-bold"}>Kasserer:</h3>
                     <p>Poul Bek</p>
-                    <Link href={"mailto: faergevej106@gmail.com"}>poul@poulbek.dk</Link>
-                    <Link href={"tel:+4526872200"}>tel. 26872200</Link>
+                    <Link className={"block underline"} href={"mailto: faergevej106@gmail.com"}>poul@poulbek.dk</Link>
+                    <Link className={"block underline"} href={"tel:+4526872200"}>tel. 26872200</Link>
                 </li>
                 <li className={"mb-6"}>
                     <h3 className={"font-bold"}>Tursejladskontakt</h3>
                     <p>Jørgen Platz</p>
-                    <Link href={"mailto:faergevej106@gmail.com"}>faergevej106@gmail.com</Link>
+                    <Link className={"block underline"} href={"mailto:faergevej106@gmail.com"}>faergevej106@gmail.com</Link>
                 </li>
                 <li className={"mb-6"}>
                     <h3 className={"font-bold"}>Kapsejladskontakt</h3>
                     <p>Jørgen Platz</p>
-                    <Link href={"mailto:faergevej106@gmail.com"}>faergevej106@gmail.com</Link>
+                    <Link className={"block underline"} href={"mailto:faergevej106@gmail.com"}>faergevej106@gmail.com</Link>
                 </li>
             </ul>
         </main>
